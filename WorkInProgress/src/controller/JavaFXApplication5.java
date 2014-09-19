b295b379dcd4b65c916cf312e3830832f4329c11
baynedus <baynedus@gmail.com>
2014-09-19 14:51:43 -0400

    updated the welcome screen and added 2 .png files

--- WorkInProgress/src/controller/JavaFXApplication5.java
+++ WorkInProgress/src/controller/JavaFXApplication5.java
@@ -1,8 +1,11 @@
 package controller;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 /**
@@ -13,11 +16,15 @@ public class JavaFXApplication5 extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
+        Image background = new Image(JavaFXApplication5.class.getResource("SpaceTrader1.png").toString());
+        ImageView back = new ImageView(background);
+        Group rot = new Group(back);
         Parent root = FXMLLoader.load(getClass().getResource("WelcomeScreen.fxml"));
-        
-        Scene scene = new Scene(root);
+        rot.getChildren().add(root);
+        Scene scene = new Scene(rot);
         
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
