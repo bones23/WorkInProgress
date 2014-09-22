e556db96d5601219d6e30289bc4c88a10ddfdf6a
Cody Fries <cfries3@gatech.edu>
2014-09-22 18:42:13 -0400

    Finished Merge of TradeItem

--- WorkInProgress/src/model/TradeItem.java
+++ WorkInProgress/src/model/TradeItem.java
@@ -12,32 +12,18 @@ package model;
  */
 
 public class TradeItem {
-<<<<<<< HEAD
-    int MTLP;   //Minimum Tech Level to Produce this resource (You can't buy on planets below this level)
-    int MTLU;   //Minimum Tech Level to Use this resource (You can't sell on planets below this level)
-    int TTP;    //Tech Level which produces the most of this item
-    int BP;     //Base Price for this resource
-    int IPL;    //Price increase per tech level
-    int Var;    //variance is the maximum percentage that the price can vary above or below the base
-    String IE;     //Radical price increase event, when this even happens on a planet, the price may increase astronomically
-    String CR;     //When this condition is present, the price of this resource is unusually low
-    String ER;     //When this condition is present, the resource is expensive
-    int MTL;    //Min price offered in space trade with random trader (not on a planet)
-    int MTH;    //Max price offered in space trade with random trader (not on a planet)
-    boolean illegal;
-=======
     private int MTLP;   //Minimum Tech Level to Produce this resource (You can't buy on planets below this level)
     private int MTLU;   //Minimum Tech Level to Use this resource (You can't sell on planets below this level)
     private int TTP;    //Tech Level which produces the most of this item
     private int BP;     //Base Price for this resource
     private int IPL;    //Price increase per tech level
     private int Var;    //variance is the maximum percentage that the price can vary above or below the base
-    private int IE;     //Radical price increase event, when this even happens on a planet, the price may increase astronomically
-    private int CR;     //When this condition is present, the price of this resource is unusually low
-    private int ER;     //When this condition is present, the resource is expensive
+    private String IE;     //Radical price increase event, when this even happens on a planet, the price may increase astronomically
+    private String CR;     //When this condition is present, the price of this resource is unusually low
+    private String ER;     //When this condition is present, the resource is expensive
     private int MTL;    //Min price offered in space trade with random trader (not on a planet)
     private int MTH;    //Max price offered in space trade with random trader (not on a planet)
->>>>>>> origin/master
+    private boolean illegal; //Whether this resource is illegal
     TradeItem(String item){
         switch(item){
             case "Water":
@@ -218,21 +204,21 @@ public class TradeItem {
     /**
      * @return the IE
      */
-    public int getIE() {
+    public String getIE() {
         return this.IE;
     }
 
     /**
      * @return the CR
      */
-    public int getCR() {
+    public String getCR() {
         return this.CR;
     }
 
     /**
      * @return the ER
      */
-    public int getER() {
+    public String getER() {
         return this.ER;
     }
 
