f2a88743403d01c2165916c3f268f3d148a7c799
Brandon Jackson <bjackson41@gatech.edu>
2014-11-10 17:09:54 -0500

    Revert "MarketPlace and Universe checkstyled. Model done"
    This reverts commit 7fce40b650d0ae93cd1da89e14f58596d83c1ebe.

--- WorkInProgress/src/model/MarketPlace.java
+++ WorkInProgress/src/model/MarketPlace.java
@@ -16,22 +16,9 @@ public class MarketPlace implements Serializable {
     private Random rand;
 
     private final int NUM_TRADE_ITEMS = 10;
-    private final double PT8 = .8;
-    private final double PT85 = .85;
-    private final double ONEPT25 = 1.25;
-    private final double ONEPT5 = 1.5;
-    private final double ONEPT75 = 1.75;
     private final int LOW_TECH = 2;
     private final int MID_TECH = 4;
     private final int HIGH_TECH = 6;
-    private final int THREE = 3;
-    private final int FOUR = 4;
-    private final int FIVE = 5;
-    private final int SIX = 6;
-    private final int SEVEN = 7;
-    private final int EIGHT = 8;
-    private final int NINE = 9;
-    private final int THIRTY = 30;
     //CHECKSTYLE: ON
 
     /**
@@ -52,13 +39,15 @@ public class MarketPlace implements Serializable {
         items[0] = new TradeItem("Water");
         items[1] = new TradeItem("Furs");
         items[2] = new TradeItem("Food");
-        items[THREE] = new TradeItem("Ore");
-        items[FOUR] = new TradeItem("Games");
-        items[FIVE] = new TradeItem("Firearms");
-        items[SIX] = new TradeItem("Medicine");
-        items[SEVEN] = new TradeItem("Machines");
-        items[EIGHT] = new TradeItem("Narcotics");
-        items[NINE] = new TradeItem("Robots");
+        //CHECKSTYLE: OFF
+        items[3] = new TradeItem("Ore");
+        items[4] = new TradeItem("Games");
+        items[5] = new TradeItem("Firearms");
+        items[6] = new TradeItem("Medicine");
+        items[7] = new TradeItem("Machines");
+        items[8] = new TradeItem("Narcotics");
+        items[9] = new TradeItem("Robots");
+        //CHECKSTYLE: ON
         amount = new int[NUM_TRADE_ITEMS];
         totalPrice = new int[NUM_TRADE_ITEMS];
         sellPrice = new int[NUM_TRADE_ITEMS];
@@ -79,19 +68,17 @@ public class MarketPlace implements Serializable {
      */
     public final void calculatePrices() {
         for (int i = 0; i < items.length; i++) {
-            totalPrice[i] = items[i].getBP() + (items[i].getIPL()
-                    * (techLevel - items[i].getMTLP()))
-                    + (int) ((double) items[i].getBP() * (items[i].getVar()));
-            sellPrice[i] = (int) ((double) totalPrice[i] * PT85);
-            sellPrice[i] = (int) ((double) totalPrice[i] * PT8);
-            if (techLevel > LOW_TECH) {
+            totalPrice[i] = items[i].getBP() + (items[i].getIPL() * (techLevel - items[i].getMTLP())) + (int)((double)items[i].getBP() * (items[i].getVar())); 
+            sellPrice[i] = (int)((double) totalPrice[i] * .85);
+            sellPrice[i] = (int)((double) totalPrice[i] * .80);
+            if (techLevel > 2) {
                 //int random = nextDouble(1.5 - 1.0 + 1.0) + 1.0;
-                totalPrice[i] = (int) (totalPrice[i] * ONEPT5);
-                sellPrice[i] = (int) (sellPrice[i] * ONEPT5);
-            } else if (techLevel > MID_TECH) {
-                totalPrice[i] = (int) (totalPrice[i] * ONEPT75);
-                sellPrice[i] = (int) (sellPrice[i] * ONEPT75);
-            } else if (techLevel > HIGH_TECH) {
+                totalPrice[i] = (int) (totalPrice[i] * 1.5);
+                sellPrice[i] = (int) (sellPrice[i] * 1.5);
+            } else if (techLevel > 4) {
+                totalPrice[i] = (int) (totalPrice[i] * 1.75);
+                sellPrice[i] = (int) (sellPrice[i] * 1.75);
+            } else if(techLevel > 6) {
                 totalPrice[i] = (int) (totalPrice[i] * 2);
                 sellPrice[i] = (int) (sellPrice[i] * 2);
             }
@@ -101,202 +88,194 @@ public class MarketPlace implements Serializable {
             if (techLevel < items[i].getMTLP()) {
                 totalPrice[i] = 0;
             }
-        }
+        }   
     }
     /**
      * Calculates the amount of items there are on each SolarSystem. Considers
      * the minimum tech level to produce.
      */
-    public final void calculateAmount() {
+    public void calculateAmount() {
         for (int i = 0; i < items.length; i++) {
             if (techLevel < items[i].getMTLP()) {
                 amount[i] = 0;
+                
             } else {
-                amount[i] = (int) Math.round((1.0
-                        - ((double) i / NUM_TRADE_ITEMS)) * (rand
-                        .nextInt(THIRTY - NUM_TRADE_ITEMS + 1)
-                                + NUM_TRADE_ITEMS));
+                amount[i] = (int)Math.round((1.0 - ((double)i / 10)) * (rand.nextInt(30 - 10 + 1) + 10 ));
                 if (techLevel == items[i].getTTP()) {
-                    amount[i] = amount[i] * 2;
+                    amount[i] = amount[i] * 2;  
                 }
             }
         }
     }
-
+    
     /**
      * Buys an item from marketplace
      * Subtracts the amount of items from market place and the amount being paid
      * from the players money.
+     * 
      * @param buttonPushed the button that was clicked
      * @param uni the Universe the the game is running on
      * @param i SolarSystem index in the Universe
      * @param s the ship to store the bought items on
      * @return the name of the item purchased
      */
-    public final String buyingItem(final String buttonPushed,
-            final Universe uni, final int i, final Ship s) {
+    public String buyingItem(String buttonPushed, Universe uni, int i, Ship s) {
         int itemNum = -1;
         String itemName = null;
-        switch(buttonPushed) {
+        switch(buttonPushed){
             case "b0":
-                itemNum = 0;
-                itemName = "Water";
+                itemNum=0;
+                itemName="Water";
                 break;
             case "b1":
-                itemNum = 1;
-                itemName = "Furs";
+                itemNum=1;
+                itemName="Furs";
                 break;
             case "b2":
-                itemNum = 2;
-                itemName = "Food";
+                itemNum=2;
+                itemName="Food";
                 break;
             case "b3":
-                itemNum = THREE;
-                itemName = "Ore";
+                itemNum=3;
+                itemName="Ore";
                 break;
             case "b4":
-                itemNum = FOUR;
-                itemName = "Games";
+                itemNum=4;
+                itemName="Games";
                 break;
             case "b5":
-                itemNum = FIVE;
-                itemName = "Firearms";
+                itemNum=5;
+                itemName="Firearms";
                 break;
             case "b6":
-                itemNum = SIX;
-                itemName = "Medicine";
+                itemNum=6;
+                itemName="Medicine";
                 break;
             case "b7":
-                itemNum = SEVEN;
-                itemName = "Machines";
+                itemNum=7;
+                itemName="Machines";
                 break;
             case "b8":
-                itemNum = EIGHT;
-                itemName = "Narcotics";
+                itemNum=8;
+                itemName="Narcotics";
                 break;
             case "b9":
-                itemNum = NINE;
-                itemName = "Robots";
+                itemNum=9;
+                itemName="Robots";
                 break;
             default:
+                
             }
-        if (uni != null && s != null && WelcomeScreenController.game.getPlayer()
-                .getMoney()
-                >= uni.getSolarSystemAt(i).getMarketPlace()
-                            .getBuyingPriceAt(itemNum)
-                && uni.getSolarSystemAt(i).getMarketPlace()
-                        .getAmountAt(itemNum) > 0
-                && s.addItem(new TradeItem(itemName))
-                && uni.getSolarSystemAt(i).getMarketPlace()
-                        .getBuyingPriceAt(itemNum) > 0) {
+        if(uni != null && s != null && WelcomeScreenController.game.getPlayer().getMoney() >=
+                    uni.getSolarSystemAt(i).getMarketPlace().getBuyingPriceAt(itemNum)
+                && uni.getSolarSystemAt(i).getMarketPlace().getAmountAt(itemNum) > 0
+                && s.addItem(new TradeItem(itemName)) == true
+                && uni.getSolarSystemAt(i).getMarketPlace().getBuyingPriceAt(itemNum) > 0){
             amount[itemNum] = amount[itemNum] - 1;
-            WelcomeScreenController.game.getPlayer()
-                    .setMoney(WelcomeScreenController.game.getPlayer()
-                            .getMoney() - totalPrice[itemNum]);
+            WelcomeScreenController.game.getPlayer().setMoney(WelcomeScreenController.game.getPlayer().getMoney() - totalPrice[itemNum]);
         }
         return itemName;
     }
-
+    
     /**
      * Sells an item to the marketplace. The market place gains an item and the
      * player gains money.
+     * 
      * @param buttonPushed the button that was clicked
      * @param uni the Universe the the game is running on
      * @param i SolarSystem index in the Universe
      * @param s the ship to store the bought items on
      * @return the name of the item sold
      */
-    public final String sellingItem(final String buttonPushed,
-            final Universe uni, final int i, final Ship s) {
+    public String sellingItem(String buttonPushed, Universe uni, int i, Ship s) {
         int itemNum = -1;
         String itemName = null;
-        switch(buttonPushed) {
+        switch(buttonPushed){
             case "s0":
-                itemNum = 0;
-                itemName = "Water";
+                itemNum=0;
+                itemName="Water";
                 break;
             case "s1":
-                itemNum = 1;
-                itemName = "Furs";
+                itemNum=1;
+                itemName="Furs";
                 break;
             case "s2":
-                itemNum = 2;
-                itemName = "Food";
+                itemNum=2;
+                itemName="Food";
                 break;
             case "s3":
-                itemNum = THREE;
-                itemName = "Ore";
+                itemNum=3;
+                itemName="Ore";
                 break;
             case "s4":
-                itemNum = FOUR;
-                itemName = "Games";
+                itemNum=4;
+                itemName="Games";
                 break;
             case "s5":
-                itemNum = FIVE;
-                itemName = "Firearms";
+                itemNum=5;
+                itemName="Firearms";
                 break;
             case "s6":
-                itemNum = SIX;
-                itemName = "Medicine";
+                itemNum=6;
+                itemName="Medicine";
                 break;
             case "s7":
-                itemNum = SEVEN;
-                itemName = "Machines";
+                itemNum=7;
+                itemName="Machines";
                 break;
             case "s8":
-                itemNum = EIGHT;
-                itemName = "Narcotics";
+                itemNum=8;
+                itemName="Narcotics";
                 break;
             case "s9":
-                itemNum = NINE;
-                itemName = "Robots";
+                itemNum=9;
+                itemName="Robots";
                 break;
             default:
-
+                
         }
-        if (uni != null && s != null && uni.getSolarSystemAt(i).getMarketPlace()
-                .getSellingPriceAt(itemNum) > 0
-                && s.removeItem(new TradeItem(itemName))) {
+        if(uni != null && s != null && uni.getSolarSystemAt(i).getMarketPlace().getSellingPriceAt(itemNum)> 0
+                && s.removeItem(new TradeItem(itemName)) == true){
             amount[itemNum] = amount[itemNum] + 1;
-            WelcomeScreenController.game.getPlayer()
-                    .setMoney(WelcomeScreenController.game.getPlayer()
-                            .getMoney() + sellPrice[itemNum]);
+            WelcomeScreenController.game.getPlayer().setMoney(WelcomeScreenController.game.getPlayer().getMoney() + sellPrice[itemNum]);
         }
         return itemName;
     }
-
+    
     /**
-     * getter for the amount.
+     * getter for the amount
      * @return amount
      */
-    public final int[] getAmount() {
+    public int[] getAmount() {
         return amount;
     }
-
+    
     /**
-     * getter for the amount at a certain index.
-     * @param index index
+     * getter for the amount at a certain index
+     * @param index
      * @return amount of a good
      */
-    public final int getAmountAt(final int index) {
+    public int getAmountAt(int index) {
         return amount[index];
     }
-
+    
     /**
-     * Getter for the selling price.
-     * @param index index
+     * Getter for the selling price
+     * @param index
      * @return selling price for an item
      */
-    public final int getSellingPriceAt(final int index) {
+    public int getSellingPriceAt(int index) {
         return sellPrice[index];
     }
-
+    
     /**
-     * Getter for the buying price of a good.
-     * @param index index
-     * @return price of a good
+     * getter for the buying price of a good
+     * @param index
+     * @return price of a good 
      */
-    public final int getBuyingPriceAt(final int index) {
+    public int getBuyingPriceAt(int index) {
         return totalPrice[index];
     }
+    
+
 }
--- WorkInProgress/src/model/Universe.java
+++ WorkInProgress/src/model/Universe.java
@@ -1,33 +1,32 @@
-package model;
-import java.io.Serializable;
-import java.util.Random;
-
 /**
  * Creates the universe of the game. The universe consists of 119 SolarSystems.
  * Also creates a 1-D array of the SolarSystem.
+ * 
  * @author Brandon Jackson
  * @version 1.0
  */
+package model;
+import java.io.Serializable;
+import java.util.Random;
 public class Universe implements Serializable {
-    //CHECKSTYLE: OFF
-        private final String[] names = {
-                    "Acamar",
-                    "Adahn",
-                    "Aldea",
-                    "Andevian",
-                    "Antedi",
-                    "Balosnee",
-                    "Baratas",
-                    "Brax",
-                    "Bretel",
-                    "Calondia",
-                    "Campor",
-                    "Capelle",
-                    "Carzon",
-		    "Castor",
+	private String[] names = {
+		    "Acamar",
+		    "Adahn",		// The alternate personality for The Nameless One in "Planescape: Torment"
+		    "Aldea",
+		    "Andevian",
+		    "Antedi",
+		    "Balosnee",
+		    "Baratas",
+		    "Brax",			// One of the heroes in Master of Magic
+		    "Bretel",		// This is a Dutch device for keeping your pants up.
+		    "Calondia",
+		    "Campor",
+		    "Capelle",		// The city I lived in while programming this game
+		    "Carzon",
+		    "Castor",		// A Greek demi-god
 		    "Cestus",
-		    "Cheron",
-		    "Courteney",
+		    "Cheron",		
+		    "Courteney",	// After Courteney Cox�
 		    "Daled",
 		    "Damast",
 		    "Davlos",
@@ -37,168 +36,164 @@ public class Universe implements Serializable {
 		    "Draylon",
 		    "Drema",
 		    "Endor",
-		    "Esmee",
+		    "Esmee",		// One of the witches in Pratchett's Discworld
 		    "Exo",
-		    "Ferris",
-		    "Festen",
-		    "Fourmi",
-		    "Frolix",
+		    "Ferris",		// Iron
+		    "Festen",		// A great Scandinavian movie
+		    "Fourmi",		// An ant, in French
+		    "Frolix",		// A solar system in one of Philip K. Dick's novels
 		    "Gemulon",
-		    "Guinifer",
-		    "Hades",
-		    "Hamlet",
-		    "Helena",
-		    "Hulst",
-		    "Iodine",
+		    "Guinifer",		// One way of writing the name of king Arthur's wife
+		    "Hades",		// The underworld
+		    "Hamlet",		// From Shakespeare
+		    "Helena",		// Of Troy
+		    "Hulst",		// A Dutch plant
+		    "Iodine",		// An element
 		    "Iralius",
-		    "Janus",
+		    "Janus",		// A seldom encountered Dutch boy's name
 		    "Japori",
 		    "Jarada",
-		    "Jason",
+		    "Jason",		// A Greek hero
 		    "Kaylon",
 		    "Khefka",
-		    "Kira",
-		    "Klaatu",
+		    "Kira",			// My dog's name
+		    "Klaatu",		// From a classic SF movie
 		    "Klaestron",
-		    "Korma",
-		    "Kravat",
+		    "Korma",		// An Indian sauce
+		    "Kravat",		// Interesting spelling of the French word for "tie"
 		    "Krios",
-		    "Laertes",
+		    "Laertes",		// A king in a Greek tragedy
 		    "Largo",
-		    "Lave",
+		    "Lave",			// The starting system in Elite
 		    "Ligon",
-		    "Lowry",
-		    "Magrat",
+		    "Lowry",		// The name of the "hero" in Terry Gilliam's "Brazil"
+		    "Magrat",		// The second of the witches in Pratchett's Discworld
 		    "Malcoria",
 		    "Melina",
-		    "Mentar",
+		    "Mentar",		// The Psilon home system in Master of Orion
 		    "Merik",
 		    "Mintaka",
-		    "Montor",
+		    "Montor",		// A city in Ultima III and Ultima VII part 2
 		    "Mordan",
-		    "Myrthe",
+		    "Myrthe",		// The name of my daughter
 		    "Nelvana",
-		    "Nix",
-		    "Nyle",
+		    "Nix",			// An interesting spelling of a word meaning "nothing" in Dutch
+		    "Nyle",			// An interesting spelling of the great river
 		    "Odet",
-		    "Og",
-		    "Omega",
-		    "Omphalos",
+		    "Og",			// The last of the witches in Pratchett's Discworld
+		    "Omega",		// The end of it all
+		    "Omphalos",		// Greek for navel
 		    "Orias",
-		    "Othello",
-		    "Parade",
+		    "Othello",		// From Shakespeare
+		    "Parade",		// This word means the same in Dutch and in English
 		    "Penthara",
-		    "Picard",
-		    "Pollux",
+		    "Picard",		// The enigmatic captain from ST:TNG
+		    "Pollux",		// Brother of Castor
 		    "Quator",
 		    "Rakhar",
-		    "Ran",
+		    "Ran",			// A film by Akira Kurosawa
 		    "Regulas",
 		    "Relva",
 		    "Rhymus",
 		    "Rochani",
-		    "Rubicum",
+		    "Rubicum",		// The river Ceasar crossed to get into Rome
 		    "Rutia",
 		    "Sarpeidon",
 		    "Sefalla",
 		    "Seltrice",
 		    "Sigma",
-		    "Sol",
+		    "Sol",			// That's our own solar system
 		    "Somari",
 		    "Stakoron",
 		    "Styris",
 		    "Talani",
 		    "Tamus",
-		    "Tantalos",
+		    "Tantalos",		// A king from a Greek tragedy
 		    "Tanuga",
 		    "Tarchannen",
 		    "Terosa",
-		    "Thera",
-		    "Titan",
-		    "Torin",
+		    "Thera",		// A seldom encountered Dutch girl's name
+		    "Titan",		// The largest moon of Jupiter
+		    "Torin",		// A hero from Master of Magic
 		    "Triacus",
 		    "Turkana",
 		    "Tyrus",
-		    "Umberlee",
-		    "Utopia",
+		    "Umberlee",		// A god from AD&D, which has a prominent role in Baldur's Gate
+		    "Utopia",		// The ultimate goal
 		    "Vadera",
 		    "Vagra",
 		    "Vandor",
 		    "Ventax",
 		    "Xenon",
-		    "Xerxes",
-		    "Yew",
-		    "Yojimbo",
+		    "Xerxes",		// A Greek hero
+		    "Yew",			// A city which is in almost all of the Ultima games
+		    "Yojimbo",		// A film by Akira Kurosawa
 		    "Zalkon",
-		    "Zuul"
+		    "Zuul"			// From the first Ghostbusters movie
 		};
+	
     private Random rand;
     private SolarSystem[] universe;
     int[][] check;
     int x;
     int y;
-
-    private final int SCREEN_X = 154;
-    private final int SCREEN_Y = 104;
-    private final int X_MAX = 146;
-    private final int Y_MAX = 96;
-    private final int THREE = 3;
-    private final int FOUR = 4;
-    //CHECKSTYLE: ON
-
+    
     /**
-     * Constructor for the Universe object. Instantiates 119 SolarSystems in
+     * Constructor for the Universe object. Instantiates 119 SolarSystems in 
      * different, random locations on a 150x100 area.
      * Makes a 1-D array of the SolarSystems.
      */
-    public Universe() {
+    public Universe(){
     	universe = new SolarSystem[names.length];
     	rand = new Random();
-    	check = new int[SCREEN_X][SCREEN_Y];
+    	check = new int[154][104];
         for (int i = 0; i < names.length; i++) {
             do {
-                x = rand.nextInt(X_MAX) + FOUR;
-                y = rand.nextInt(Y_MAX) + FOUR;
+                x = rand.nextInt(146) + 4;
+                y = rand.nextInt(96)+4;
             } while(check[x][y] == 1);
             universe[i] = new SolarSystem(names[i], x, y);
             check[x][y] = 1;
-            for (int j = x - THREE; j < x + THREE; j++) {
-                for (int k = y - THREE; k < y + THREE; k++) {
+            for (int j = x - 3; j < x + 3; j++) {
+                for (int k = y-3; k < y + 3; k++) {
                     check[j][k] = 1;
                 }
             }
         }
     }
-
+    
     /**
      * Prints the universe through the console.
+     * 
      * @return  toString - the Universe's information for each SolarSystem in it
      */
-    @Override
-    public final String toString() {
+    public String toString() {
         String toString = "";
-        for (SolarSystem universe1 : this.universe) {
-            toString += (universe1.toString() + "\n");
-        }
+    	for (int i = 0; i < this.universe.length; i++) {
+            toString += (this.universe[i].toString() + "\n");
+    	}
         return toString;
     }
-
+    
     /**
-     * Getter for the universe. Returns a 1-D array of SolarSystems which make
+     * Getter for the universe. Returns a 1-D array of SolarSystems which make 
      * up the universe.
+     * 
      * @return universe - collection of SolarSystems
      */
-    public final SolarSystem[] getUniverse() {
+    public SolarSystem[] getUniverse() {
         return universe;
     }
-
+    
     /**
      * getter for a SolarSystem at a specified index.
      *
-     * @param index INDINEIXNEINXIDN
+     * @param index
      * @return SolarSystem at index
      */
-    public final SolarSystem getSolarSystemAt(final int index) {
+    public SolarSystem getSolarSystemAt(int index) {
         return universe[index];
     }
+    
+    
 }
